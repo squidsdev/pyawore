83d23754b5c028326026ed08a49d6ca2eac487a6
squidsdev <130568713+squidsdev@users.noreply.github.com>
2023-04-12 16:05:14 +0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+import setuptools
+
+setup(
+    name='pyawore',
+    version='0.1',
+    author='squids',
+    description='Awore simple api',
+    long_description='See https://github.com/squidsdev/pyawore',
+    url='https://github.com/squidsdev/pyawore',
+    install_requires=['requests', 'uuid'],
+    packages=setuptools.find_packages()
+)
