db05587df8eaf497dd079645f6924b90224896e9
squidsdev <130568713+squidsdev@users.noreply.github.com>
2024-10-12 19:32:20 +0300

    Improved code readability

--- README.md
+++ README.md
@@ -6,48 +6,55 @@ pip install git+https://github.com/squidsdev/pyawore
 ```
 ## Использование
 ### Получение баланса токенов
+
 ```python
 from pyawore import *
 
-awore_token = "ВАШ ТОКЕН" # https://awore.ru/settings
-awore = pyawore(awore_token)
+awore_token = "ВАШ ТОКЕН"  # https://awore.ru/settings
+awore = Pyawore(awore_token)
 
 print(awore.get_all_balance())
 ```
 ### Отправка средств с токенов
+
 ```python
 from pyawore import *
 
-awore_token = "ВАШ ТОКЕН" # https://awore.ru/settings
-awore = pyawore(awore_token)
+awore_token = "ВАШ ТОКЕН"  # https://awore.ru/settings
+awore = Pyawore(awore_token)
 
-awore.send_transaction(token="BTC", address_to="0x563baA79711AEb43e8f35917c427A54cA66Bb2cB", amount=10, includeFee=False)
+awore.send_transaction(token="BTC", address_to="0x563baA79711AEb43e8f35917c427A54cA66Bb2cB", amount=10,
+    includeFee=False)
 ```
 ### Создание счета
+
 ```python
 from pyawore import *
 
-awore_token = "ВАШ ТОКЕН" # https://awore.ru/settings
-awore = pyawore(awore_token)
+awore_token = "ВАШ ТОКЕН"  # https://awore.ru/settings
+awore = Pyawore(awore_token)
 
-payment = awore.create_invoice(amount=100, comment='Покупка котенка', return_url='https://awore.ru', callback_url='https://awore.ru/id?=4234235', token_code='BTC', currency_code='USD')
+payment = awore.create_invoice(amount=100, comment='Покупка котенка', return_url='https://awore.ru',
+    callback_url='https://awore.ru/id?=4234235', token_code='BTC', currency_code='USD')
 print(payment["url"])
 ```
 ### Получение инфорации о счете
+
 ```python
 from pyawore import *
 
-awore_token = "ВАШ ТОКЕН" # https://awore.ru/settings
-awore = pyawore(awore_token)
+awore_token = "ВАШ ТОКЕН"  # https://awore.ru/settings
+awore = Pyawore(awore_token)
 
 print(awore.invoice_info(id=143564))
 ```
 ### Получение всех счетов
+
 ```python
 from pyawore import *
 
-awore_token = "ВАШ ТОКЕН" # https://awore.ru/settings
-awore = pyawore(awore_token)
+awore_token = "ВАШ ТОКЕН"  # https://awore.ru/settings
+awore = Pyawore(awore_token)
 
 print(awore.invoice_all())
 ```
--- pyawore/Session.py
+++ pyawore/Session.py
@@ -1,61 +1,68 @@
 from __future__ import annotations
-from pyawore import AuthError, SendTransactionError, CreateInvoiceError
-from requests import get, post
 import json
+from requests import post
+from pyawore import AuthError, SendTransactionError, CreateInvoiceError
 
-class pyawore(object):
-	def __init__(self, key: str) -> pyawore:
-		self.token = key
-		self.headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
-		self.url = "https://awore.ru/api/v2"
 
-		self.check_key()
+class Pyawore(object):
+    def __init__(self, key: str) -> None:
+        self.token = key
+        self.headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
+        self.url = "https://awore.ru/api/v2"
 
-	def check_key(self) -> bool:
-		if post(self.url + "/token/getAllBalance", headers=self.headers, data=json.dumps({"tokenApi":self.token})).json().get('statusCode'):
-			raise AuthError("Неверный ключ API (Invalid API key)")
-		return True
+        self.check_key()
 
-	def get_all_balance(self) -> dict:
-		return post(self.url + "/token/getAllBalance", headers=self.headers, data=json.dumps({"tokenApi":self.token})).json()
+    def check_key(self) -> bool:
+        response = post(self.url + "/token/getAllBalance", headers=self.headers, data=json.dumps({"tokenApi": self.token})).json()
+        if response.get('statusCode') != 200:
+            raise AuthError("Неверный ключ API (Invalid API key)")
+        return True
 
-	def send_transaction(self, token: str, address_to: str, amount: float, includeFee: bool = False) -> dict:
-		data = {
-			"tokenApi": self.token,
-			"token": token,
-			"address_to": address_to,
-			"amount": amount,
-			"includeFee": includeFee
-		}
-		data = post(self.url + "/token/sendTransaction", headers=self.headers, data=json.dumps(data)).json()
+    def get_all_balance(self) -> dict:
+        response = post(self.url + "/token/getAllBalance", headers=self.headers, data=json.dumps({"tokenApi": self.token}))
+        return response.json()
 
-		if data.get('error'):
-			raise SendTransactionError(data["message"])
-	def create_invoice(self, amount: float, token_code: str, currency_code: str, comment: str = '', return_url: str = None, callback_url: str = None) -> dict:
-		data = {
-			"tokenApi": self.token,
-			"amount": amount,
-			"comment": comment,
-			"returnUrl": return_url,
-			"callbackUrl": callback_url,
-			"tokenCode": token_code,
-			"currencyCode": currency_code
-		}
-		data = post(self.url + "/invoice/create", headers=self.headers, data=json.dumps(data)).json()
+    def send_transaction(self, token: str, address_to: str, amount: float, includeFee: bool = False) -> dict:
+        data = {
+            "tokenApi": self.token,
+            "token": token,
+            "address_to": address_to,
+            "amount": amount,
+            "includeFee": includeFee
+        }
+        response = post(self.url + "/token/sendTransaction", headers=self.headers, data=json.dumps(data)).json()
 
-		if data.get('error'):
-			raise CreateInvoiceError(data["message"])
-		return data
+        if 'error' in response:
+            raise SendTransactionError(response["message"])
+        return response
 
-	def invoice_info(self, id: int) -> dict:
-		data = {
-			"tokenApi": self.token,
-			"id": id,
-		}
-		return post(self.url + "/invoice/info", headers=self.headers, data=json.dumps(data)).json()
+    def create_invoice(self, amount: float, token_code: str, currency_code: str, comment: str = '', return_url: str = None, callback_url: str = None) -> dict:
+        data = {
+            "tokenApi": self.token,
+            "amount": amount,
+            "comment": comment,
+            "returnUrl": return_url,
+            "callbackUrl": callback_url,
+            "tokenCode": token_code,
+            "currencyCode": currency_code
+        }
+        response = post(self.url + "/invoice/create", headers=self.headers, data=json.dumps(data)).json()
 
-	def invoice_all(self) -> dict:
-		data = {
-			"tokenApi": self.token
-		}
-		return post(self.url + "/invoice/all", headers=self.headers, data=json.dumps(data)).json()
+        if 'error' in response:
+            raise CreateInvoiceError(response["message"])
+        return response
+
+    def invoice_info(self, id: int) -> dict:
+        data = {
+            "tokenApi": self.token,
+            "id": id,
+        }
+        response = post(self.url + "/invoice/info", headers=self.headers, data=json.dumps(data))
+        return response.json()
+
+    def invoice_all(self) -> dict:
+        data = {
+            "tokenApi": self.token
+        }
+        response = post(self.url + "/invoice/all", headers=self.headers, data=json.dumps(data))
+        return response.json()
